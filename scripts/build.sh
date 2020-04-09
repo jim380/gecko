d8806039aeeca00637f3a9d91957ae8ac327acb6
jim380 <jyz1992@hotmail.com>
2020-04-09 02:32:35 -0700

    cross-compiling for arm (in progress)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,25 @@
 #!/bin/bash -e
 
 # Ted: contact me when you make any changes
+export SALTICIDAE_GO_PATH="$GOPATH/src/github.com/$SALTICIDAE_ORG/salticidae-go"
+export SALTICIDAE_PATH="$SALTICIDAE_GO_PATH/salticidae"
+export CGO_CFLAGS="-I$SALTICIDAE_PATH/include/salticidae"
+#export CGO_LDFLAGS="-L$SALTICIDAE_PATH/build/lib/ -lsalticidae -luv -lssl -lcrypto -lstdc++"
 
 PREFIX="${PREFIX:-$(pwd)/build}"
 
 SRC_DIR="$(dirname "${BASH_SOURCE[0]}")"
-source "$SRC_DIR/env.sh"
+#source "$SRC_DIR/env.sh"
 
 GECKO_PKG=github.com/ava-labs/gecko
 GECKO_PATH="$GOPATH/src/$GECKO_PKG"
 if [[ -d "$GECKO_PATH/.git" ]]; then
     cd "$GECKO_PATH"
-    go get -t -v "./..."
+    CGO_CFLAGS="-I$SALTICIDAE_PATH/include/salticidae" go get -t -v "./..."
     cd -
 else
-    go get -t -v "$GECKO_PKG/..."
+    CGO_CFLAGS="-I$SALTICIDAE_PATH/include/salticidae" go get -t -v "$GECKO_PKG/..."
 fi
-go build -o "$PREFIX/ava" "$GECKO_PATH/main/"*.go
-go build -o "$PREFIX/xputtest" "$GECKO_PATH/xputtest/"*.go
+
+env GOOS=linux GOARCH=arm GOARM=7 CGO_ENABLED=1 CC=arm-linux-gnueabihf-gcc go build -o "$PREFIX/ava" "$GECKO_PATH/main/"*.go
+env GOOS=linux GOARCH=arm GOARM=7 CGO_ENABLED=1 CC=arm-linux-gnueabihf-gcc go build -o "$PREFIX/xputtest" "$GECKO_PATH/xputtest/"*.go
